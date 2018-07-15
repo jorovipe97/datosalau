da29f2e49570ed6f2ff1af7da123cc049b1ad647
romualdo97 <jorovipe@gmail.com>
2018-07-15 18:33:19 -0500

    First usable version of the app finished

--- google-geocharts/js/app.js
+++ google-geocharts/js/app.js
@@ -79,6 +79,61 @@ let App = {
     return changed;
   },
 
+  removeLatinCharsAndSpaces: function (original) {
+    let changed = null;
+
+    if (original === 'Distrito Capital de Bogotá'.toUpperCase()) {
+      changed = 'BOGOTADC';
+    }
+    else if (original === 'Atlántico'.toUpperCase()) {
+      changed = 'ATLANTICO';
+    }
+    else if (original === 'Bolívar'.toUpperCase()) {
+      changed = 'BOLIVAR';
+    }
+    else if (original === 'Boyacá'.toUpperCase()) {
+      changed = 'BOYACA';
+    }
+    else if (original === 'Caquetá'.toUpperCase()) {
+      changed = 'CAQUETA';
+    }
+    else if (original === 'Córdoba'.toUpperCase()) {
+      changed = 'CORDOBA';
+    }
+    else if (original === 'Chocó'.toUpperCase()) {
+      changed = 'CHOCO';
+    }
+    else if (original === 'Guainía'.toUpperCase()) {
+      changed = 'GUAINIA';
+    }
+    else if (original === 'La Guajira'.toUpperCase()) {
+      changed = 'LAGUAJIRA';
+    }
+    else if (original === 'Nariño'.toUpperCase()) {
+      changed = 'NARINO';
+    }
+    else if (original === 'Norte de Santander'.toUpperCase()) {
+      changed = 'NORTEDESANTANDER';
+    }
+    else if (original === 'Quindío'.toUpperCase()) {
+      changed = 'QUINDIO';
+    }
+    else if (original === 'San Andrés, Providencia y Santa Catalina'.toUpperCase()) {
+      changed = 'ARCHIPIELAGODESANDRESYPROVIDENCIA';
+    }
+    else if (original === 'Valle del Cauca'.toUpperCase()) {
+      changed = 'VALLEDELCAUCA';
+    }
+    else if (original === 'Vaupés'.toUpperCase()) {
+      changed = 'VAUPES';
+    }
+    else {
+      changed = original;
+    }
+
+    return changed;
+  },
+
   isChartInitialized: function () {
 
   },
@@ -94,7 +149,7 @@ $(function () { // This is called by jquery when document is loaded
 
   // Colombian heatmap example: http://www.danielpinero.com/como-crear-mapa-calor-colombia
   google.charts.load('current', {
-    'packages': ['geochart', 'table'],
+    'packages': ['geochart', 'table', 'corechart'],
     // Note: you will need to get a mapsApiKey for your project.
     // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
     'mapsApiKey': 'AIzaSyAaxlmWR-_VWkr1qrFIOUurltV6EvulMLo'
@@ -112,9 +167,16 @@ $(function () { // This is called by jquery when document is loaded
   var chart = null;
   // Table object
   var table = null;
+  // Municipios data
+  var municipiosData = null;
+  let clickedDepartamento = null;
+
 
   // used for show total visits by department
   var geochartView = null;
+
+  var lineChart = null;
+
 
   var column = 4;
   function drawRegionsMap() {
@@ -126,17 +188,7 @@ $(function () { // This is called by jquery when document is loaded
       ['Mesi', 33, 52, 'Argentina'],
       ['Radamel', 80, 30, 'Colombia']
     ]
-    var dummyData = google.visualization.arrayToDataTable(foo);
-    table = new google.visualization.Table(document.getElementById('table'));
-    tableOptions = {};
-
-    // https://developers.google.com/chart/interactive/docs/examples
-    var view = new google.visualization.DataView(dummyData);
-    view.setColumns([0, 1, 3]);
-    console.log(dummyData);
-    view.setRows(view.getFilteredRows([{column: 2, value: 'Colombia'}]));
-    view.setColumns([0, 1]);
-    //table.draw(view, tableOptions);
+    
     
     // Adds tittles to the dataset for make it usable for arrayToDatable
     // Iterates over the visits of all the dates
@@ -152,11 +204,17 @@ $(function () { // This is called by jquery when document is loaded
 
       // Adds a title to the department visits table
       // unshift() adds elements to the beginning of the 'departamentos' array
-      visitantes[i].departamentos.unshift(['Departamento', 'Terrestres', 'Nacionales aereos', 'Internacionales aereos', 'Visitantes totales']);
+      visitantes[i].departamentos.unshift(['Departamento', 'Terrestres', 'Nacionales aéreos', 'Internacionales aéreos', 'Visitantes totales']);
 
       // Adds title to the 'municipio' visits table
-      visitantes[i].municipios.unshift(['Municipio', 'Terrestres', 'Nacionales aereos', 'Internacionales aereos', 'Visitantes totales']);
+      visitantes[i].municipios.unshift(['Municipio', 'Terrestres', 'Nacionales aéreos', 'Internacionales aéreos', 'Visitantes totales', 'Departamento']);
     }
+
+
+    municipiosData = google.visualization.arrayToDataTable(visitantes[0].municipios);
+    table = new google.visualization.Table(document.getElementById('table'));
+    tableOptions = {};    
+
 
     // Nota: El nombre de los departamentos es sensible a tildes
     // NOTA: El nombre de los departamentos no es sensible a mayusculas
@@ -165,7 +223,7 @@ $(function () { // This is called by jquery when document is loaded
     geochartView = new google.visualization.DataView(data);
     geochartView.setColumns([0, column]); // Selects 'Nombre departamento' and 'Visitas totales' from the table
     //table.draw(geochartView, tableOptions);    
-    console.log(visitantes[0].departamentos);
+    // console.log(visitantes[0].departamentos);
 
     options = {
       region: 'CO', // A country, specified by its ISO 3166-1 alpha-2 
@@ -181,13 +239,98 @@ $(function () { // This is called by jquery when document is loaded
 
     /* Draws the initial map information */
     chart.draw(geochartView, options);
-    table.draw(data, tableOptions);
+
+    // Instantiates the line chart
+    lineChart = new google.visualization.LineChart(document.getElementById('line_chart'));
+
 
     // Register callback for region click
     google.visualization.events.addListener(chart, 'select', function () {
       var selection = chart.getSelection();
       if (selection.length > 0) {
-        console.log(data.getValue(selection[0].row, 0) + ' selected!');
+        // Gets the name of the selected departamento
+        clickedDepartamento = data.getValue(selection[0].row, 0);
+        // console.log(clickedDepartamento + ' selected!');
+
+        $('.departamento-name').html(clickedDepartamento);
+
+        // https://developers.google.com/chart/interactive/docs/examples
+        var tableView = new google.visualization.DataView(municipiosData);
+        tableView.setColumns([0, 1, 2, 3, 4, 5]);
+        // console.log(dummyData);
+        tableView.setRows(tableView.getFilteredRows([{column: 5, value: App.removeLatinCharsAndSpaces(clickedDepartamento)}]));
+        tableView.setColumns([0, 1, 2, 3, 4]);
+        //table.draw(view, tableOptions);
+        table.draw(tableView, tableOptions);
+
+        var timeSeries = [];
+        // Generating TIME SERIES for departamentos for each date
+        // Iterating over each date
+        for (let i = 0; i < visitantes.length; i++) {
+          let obj = visitantes[i];
+          let date = obj.date;
+
+          let timeSlice = [];
+          
+          // Iterating over each department
+          for (let j = 0; j < obj.departamentos.length; j++) {
+            // Gets the data of the clicked department
+            if (obj.departamentos[j][0] === clickedDepartamento) {
+              // Copies the data array for this departamento in the current time
+              timeSlice = obj.departamentos[j].slice();
+
+              // Removes the first item from the array (the 'departamento' column)
+              timeSlice.shift();
+              // Removes total column
+              timeSlice.pop();
+
+              // Converts date string to date object
+              let dateSplit = date.split("-");
+              // Date month starts at 0 not at 1.
+              let dateObj = new Date(parseInt(dateSplit[0]), parseInt(dateSplit[1])-1, 1);
+
+              // Adds the date to timeSlice data
+              timeSlice.unshift(i);
+
+              // Adds time slice to time serie
+              timeSeries.push(timeSlice);
+
+
+              // Exits of the inner for loop
+              break;
+            }
+          }
+        }
+
+        // Adds column names to the time series array
+        timeSeries.unshift(['Fecha', 'Terrestres', 'Nacionales aéreos', 'Internacionales aéreos'])
+        // console.log(timeSeries);
+
+        var lineChartOptions = {
+          title: 'Serie de tiempo',
+          legend: { position: 'bottom' },
+          width: 720,
+          height: 480          
+        };
+        lineChartOptions.title = `Visitantes por medio de transporte en ${clickedDepartamento}`;
+
+        // Creates a data table from the array generated
+        lineChartDataTable = google.visualization.arrayToDataTable(timeSeries);
+
+        // Draw the time series
+        lineChart.draw(lineChartDataTable, lineChartOptions);
+
+        // Checks the element has the visible class
+        if ($('#tip-click').hasClass('visible'))
+        {
+          // Removes tip about click over departamento
+          $('#tip-click').removeClass('visible');
+        }
+
+        // Animates scroll until details https://www.abeautifulsite.net/smoothly-scroll-to-an-element-without-a-jquery-plugin-2
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#details").offset().top
+        }, 600);
       }
     });
   }
@@ -213,9 +356,18 @@ $(function () { // This is called by jquery when document is loaded
     data = google.visualization.arrayToDataTable(visitantes[this.value].departamentos);
     geochartView = new google.visualization.DataView(data);
     geochartView.setColumns([0, column]); // Selects 'Nombre departamento' and 'Visitas totales' from the table
-
     chart.draw(geochartView, options);
-    table.draw(data, tableOptions);
+
+
+    municipiosData = google.visualization.arrayToDataTable(visitantes[this.value].municipios);
+    // https://developers.google.com/chart/interactive/docs/examples
+    var tableView = new google.visualization.DataView(municipiosData);
+    tableView.setColumns([0, 1, 2, 3, 4, 5]);
+    // console.log(dummyData);
+    tableView.setRows(tableView.getFilteredRows([{column: 5, value: App.removeLatinCharsAndSpaces(clickedDepartamento)}]));
+    tableView.setColumns([0, 1, 2, 3, 4]);
+    //table.draw(view, tableOptions);
+    table.draw(tableView, tableOptions);
 
   }
 
@@ -246,5 +398,19 @@ $(function () { // This is called by jquery when document is loaded
 
     return true;
   }
+
+  $('#up-button').click(function () {
+    // Animates scroll until details https://www.abeautifulsite.net/smoothly-scroll-to-an-element-without-a-jquery-plugin-2
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#dashboard").offset().top
+    }, 600);
+  });
+
+  $('#goto-datasets-btn').bind('click', function () {
+    // Animates scroll until details https://www.abeautifulsite.net/smoothly-scroll-to-an-element-without-a-jquery-plugin-2
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#used-datasets").offset().top
+    }, 600);
+  });
 });
 
